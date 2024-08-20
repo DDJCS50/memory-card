187c8360d27773ca54cd06212264b7b24200f8db
djns <130430285+DDJCS50@users.noreply.github.com>
2024-08-20 09:36:49 -0700

    Add header and main functionality
    Added header content that displays the game goal
    and the game scores. Added basic main content that
    will contain cards for the game and increments the
    score when clicked.

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,14 +1,56 @@
 import "../styles/main.css";
+import { useState } from "react";
 
 function Page() {
+  const [score, setScore] = useState(0);
+
   return (
     <>
       <header>
-        <h1>Memory Card Game</h1>
+        <div className="title">
+          <h1>Memory Card Game</h1>
+          <p>Catch em all without catching two of the same!</p>
+        </div>
+        <Score playerScore={score}></Score>
       </header>
-      <main></main>
+      <main>
+        <CardContainer score={score} setScore={setScore}></CardContainer>
+      </main>
       <footer></footer>
     </>
+  );
+}
+
+function Score({ playerScore }) {
+  let highScore = 10;
+
+  return (
+    <div className="scoreContainer">
+      <h2>Score: {playerScore}</h2>
+      <h2>High Score: {highScore}</h2>
+    </div>
+  );
+}
+
+function CardContainer({ score, setScore }) {
+  return (
+    <div className="cardBox">
+      <Card score={score} setScore={setScore}></Card>
+      <Card score={score} setScore={setScore}></Card>
+      <Card score={score} setScore={setScore}></Card>
+      <Card score={score} setScore={setScore}></Card>
+    </div>
+  );
+}
+
+function Card({ score, setScore }) {
+  return (
+    <div
+      className="card"
+      onClick={() => {
+        setScore(score + 1);
+      }}
+    ></div>
   );
 }
 
